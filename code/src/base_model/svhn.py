5e2357672d127e29580c3feaaaf21e3cb04a75fc
HerlufFraDiku <herlufbaggesen13@gmail.com>
2022-10-26 13:40:40 +0200

    add svhn.py file

---
+++ code/src/base_model/svhn.py
@@ -0,0 +1,36 @@
+import os
+import pandas as pd
+from torch.utils.data import Dataset
+from torchvision.io import read_image
+
+
+class SVHN(Dataset):
+
+    def __init__(self, train=True, transform=None, target_transform=None):
+        self.transform = transform
+        self.target_transform = target_transform
+
+        if not train:
+            print("JK we aint got no test data")
+            exit()
+
+        self.img_dir = os.path.join("datasets",
+                                    "train") if train else os.path.join(
+                                        "datasets", "test")
+
+        self.img_labels = pd.read_csv(os.path.join(self.img_dir, "bbox.csv"))
+
+    def __len__(self):
+        return len(self.img_labels)
+
+    def __getitem__(self, idx):
+        img_path = os.path.join(self.img_dir, self.img_labels.iloc[idx, 0])
+        image = read_image(img_path)
+        label = self.img_labels.loc[self.img_labels["FileName"].is(str(idx) + ".png")]
+
+        if self.transform:
+            image = self.transform(image)
+        if self.target_transform:
+            label = self.target_transform(label)
+
+        return image, label
