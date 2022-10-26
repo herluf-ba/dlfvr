90f503183d6723c2e1fa09efbac48beabe7f023d
HerlufFraDiku <herlufbaggesen13@gmail.com>
2022-10-26 13:38:06 +0200

    begin implementing custom dataset

--- code/src/base_model/__main__.py
+++ code/src/base_model/__main__.py
@@ -1,7 +1,9 @@
+import os
+from matplotlib import pyplot as plt
 import torch as t
 import numpy as np
 from torch.utils.data import DataLoader
-from torchvision.datasets import SVHN
+from svhn import SVHN
 import torch.nn.functional as F
 from training import fit
 from torchvision.transforms import Compose, ToTensor, Grayscale, Resize
@@ -17,18 +19,29 @@ loss_func = F.mse_loss
 # Transform input images
 transform = Compose([Grayscale(), Resize((64, 64)), ToTensor()])
 
-svhn_train = SVHN(root="../../datasets",
-                  split="train",
-                  download=True,
-                  transform=transform)
-svhn_test = SVHN(root="../../datasets",
-                 split="test",
-                 download=True,
-                 transform=transform)
+svhn_train = SVHN(train=True)
+
+for img, label in svhn_train:
+    plt.imshow(img.numpy().transpose([1, 2, 0]))
+    print(f"{label=}")
+    plt.show()
+    break
+
+# svhn_train = SVHN(root=os.path.join(os.getcwd(), "..", "datasets"),
+# split="train",
+# download=True,
+# transform=transform)
+# svhn_test = SVHN(root=os.path.join(os.getcwd(), "..", "datasets"),
+# split="test",
+# download=True,
+# transform=transform)
 
 # TODO: Use SVHN data set here
 train = DataLoader(svhn_train, batch_size=batch_size)
-test = DataLoader(svhn_test)
+test = DataLoader(svhn_train)
+
+X, y = svhn_train[0]
+print(f"{y.shape=}")
 
 model = BaseModel()
 opt = t.optim.SGD(model.parameters(), lr=learning_rate,
